53ba9754abfbe384fd9cdb1e4dbcb90b964b096a
liyuan Huang <liyuan Huang>
2017-05-26 10:48:06 +0800

    commit

--- main/student.js
+++ main/student.js
@@ -1,3 +1,10 @@
 let Person = require('./person')
 // Write your code here
-module.exports = Student
+module.exports = Student(name,age,kclass){
+	Person.call(this,name,age);
+	this.kclass = kclass;
+	this.introduce=function(){
+		return "My name is "+name+". I am "+age+" years old."+" I am at Class "+kclass+" .";
+	}
+}
+
